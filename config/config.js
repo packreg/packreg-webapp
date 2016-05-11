2339df646349d36c16c98954b845bbfc7644248d
Ben <benmann89@gmail.com>
2016-05-11 21:42:44 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ config.port = 3434;
 config.defaultSize = 30;
 config.isPublic = true;
 
-config.apiPath = 'http://packetregistry.com/model.json';
+config.apiPath = 'http://all.packetregistry.com/model.json';
 
 
 module.exports = config;
